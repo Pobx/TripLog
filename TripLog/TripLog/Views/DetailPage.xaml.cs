bff3f36526aa9f46c29740427ca92c969e07906f
11430279 <geidtiphong@gmail.com>
2023-01-03 17:01:30 +0700

    feat: add logic for check lat long before show map

--- TripLog/TripLog/Views/DetailPage.xaml.cs
+++ TripLog/TripLog/Views/DetailPage.xaml.cs
@@ -6,6 +6,7 @@ using TripLog.Models;
 using Xamarin.Forms.Maps;
 using TripLog.ViewModels;
 using TripLog.Services;
+using System.ComponentModel;
 
 namespace TripLog.Views
 {
@@ -19,19 +20,54 @@ namespace TripLog.Views
         public DetailPage()
         {
             InitializeComponent();
-            BindingContext = new DetailViewModel(DependencyService.Get<INavService>());
+        }
 
+        void UpdateMap()
+        {
+            if (_vm.Entry == null)
+            {
+                return;
+            }
+
+            //Center the map around the log entry's location
             map.MoveToRegion(MapSpan.FromCenterAndRadius(
-                new Position(_vm.Entry.Latitude, _vm.Entry.Longitude)
-                , Distance.FromMiles(.5)
-                ));
+                new Position(_vm.Entry.Latitude, _vm.Entry.Longitude), Distance.FromMiles(.5)));
 
+            //Place a pin on the map for the log entry's location
             map.Pins.Add(new Pin
             {
                 Type = PinType.Place,
                 Label = _vm.Entry.Title,
                 Position = new Position(_vm.Entry.Latitude, _vm.Entry.Longitude)
             });
+        }
+
+        void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == nameof(DetailViewModel.Entry))
+            {
+                UpdateMap();
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_vm != null)
+            {
+                _vm.PropertyChanged += OnViewModelPropertyChanged;
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (_vm != null)
+            {
+                _vm.PropertyChanged -= OnViewModelPropertyChanged;
+            }
         }
     }
 }
