3a7127be503fe39157c7804aaef642a0dc026b34
11430279 <geidtiphong@gmail.com>
2023-01-04 18:09:48 +0700

    feat: move this file to ViewModel directory

--- TripLog/TripLog/ViewModels/BaseViewModel.cs
+++ TripLog/TripLog/ViewModels/BaseViewModel.cs
@@ -4,25 +4,24 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using TripLog.Services;
 
-namespace TripLog.Views
+namespace TripLog.ViewModels
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         protected INavService NavService { get; private set; }
 
-        protected BaseViewModel(INavService navService) : base()
+        protected BaseViewModel(INavService navService)
         {
             NavService = navService;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public abstract Task Init();
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        public abstract Task Init();
     }
 
     public abstract class BaseViewModel<TParameter> : BaseViewModel
